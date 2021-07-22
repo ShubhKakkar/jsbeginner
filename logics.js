9c8212ba27e9357fd3cb7a014a2a0f1e72dea43e
ShubhKakkar <86162630+ShubhKakkar@users.noreply.github.com>
2021-07-22 16:07:18 +0530

    Tabs

--- logics.js
+++ logics.js
@@ -11,3 +11,39 @@ sluggify("I am Shubham Kakkar",true);
 
 consoe.log(typeof(1000));
 console.log(typeof("name"));
+
+console.log(10==10);
+if(name == "scott" || (name == "wes" && last == "bos")){
+    console.log("Cool Name");
+}
+const sluggify = (text,lower) => {
+    if(lower){
+        return text.toLowerCase();
+    }
+    else{
+        return text;
+    }
+}
+console.log(true || true || false || false);
+console.log(true && false && false && true);
+Object.keys({name:"wes"}).length
+//This will return 1 as the answer as the Object is only having one key value name:"wes".
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.keys(object1));
+  // expected output: Array ["a", "b", "c"]
+
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.keys(object1));
+  // expected output: Array ["a", "b", "c"]
+
