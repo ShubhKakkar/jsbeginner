9dd90bf164f21f5a648d30c7e6606987bb76ad80
ShubhKakkar <86162630+ShubhKakkar@users.noreply.github.com>
2021-07-22 19:22:22 +0530

    changes

--- object.js
+++ object.js
@@ -8,3 +8,6 @@ const obj = {
   obj.id = 4;
   console.log(obj.prop);
   console.log(obj.id);
+
+  // const person3 = person1 , this will not copy data of person 1 but will point on person1 . It means if we change the values of person 3 , the values of person 1 will change too.
+  // const person3 = { ...person1 }; => This is the way to create a copy named person3 of the object person1.
